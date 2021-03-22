c7415796308fb2ef0f19a8448844c60a947823f4
Luismi <luismiac93@gmail.com>
2021-03-22 09:54:39 -0500

    seperacion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,44 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
-import { PagesComponent } from './pages/pages.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+import { PagesRoutingModule } from './pages/pages.routing.module';
+import { AuthRoutingModule } from './auth/auth.routing.module';
 
 const routes: Routes = [
   {
     path: '',
-    component: PagesComponent,
-    children: [
-      {
-        path: 'dashboard',
-        component: DashboardComponent,
-      },
-      {
-        path: 'progress',
-        component: ProgressComponent,
-      },
-      {
-        path: 'grafica1',
-        component: Grafica1Component,
-      },
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full',
-      },
-    ]
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'register',
-    component: RegisterComponent,
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
   },
   {
     path: '**',
@@ -54,6 +24,8 @@ const routes: Routes = [
   ],
   exports: [
     RouterModule,
+    PagesRoutingModule,
+    AuthRoutingModule,
   ]
 })
 export class AppRoutingModule { }
